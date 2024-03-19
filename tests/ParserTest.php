8d4938c074b54449dd6e517c5ff7ae5cc61c2f37
Aerendir <hello@aerendir.me>
2024-03-19 17:19:08 +0100

    Apply fixes.

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -27,19 +27,19 @@ final class ParserTest extends TestCase
 
         // This is the first result of the parsed file
         $expected = [
-            0 => 'IT',
-  1           => '67010',
-  2           => 'Barete',
-  3           => 'Abruzzi',
-  4           => 'AB',
-  5           => "L'Aquila",
-  6           => 'AQ',
-  7           => '',
-  8           => '',
-  9           => '42.4501',
-  10          => '13.2806',
-  11          => '4',
-];
+            0           => 'IT',
+            1           => '67010',
+            2           => 'Barete',
+            3           => 'Abruzzi',
+            4           => 'AB',
+            5           => "L'Aquila",
+            6           => 'AQ',
+            7           => '',
+            8           => '',
+            9           => '42.4501',
+            10          => '13.2806',
+            11          => '4',
+        ];
         $result = Parser::parse($filePath);
 
         self::assertCount(17980, $result);
